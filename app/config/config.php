511d1a600f3d6635078fb573af747f43df5dc9c7
Ruben Cid <rubencidlara@gmail.com>
2016-01-12 16:49:48 +0100

    openshift

--- app/config/config.php
+++ app/config/config.php
@@ -42,7 +42,7 @@ if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
  * then replace this line with full URL (and sub-folder) and a trailing slash.
  */
 define('URL_PUBLIC_FOLDER', 'public');
-define('URL_PROTOCOL', 'http://');
+define('URL_PROTOCOL', 'https://');
 define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
 define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
 define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
