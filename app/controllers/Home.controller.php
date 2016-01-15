6ea05c08bb9307614a4276ed0835a54d49ad0c5c
Ruben Cid <rubencidlara@gmail.com>
2016-01-15 19:12:47 +0100

    test curl

--- app/controllers/Home.controller.php
+++ app/controllers/Home.controller.php
@@ -17,6 +17,10 @@
           // load VIEWSs
           $productos = $this->product->toArray();
           $currentUser = self::getSession('username');
+          $handler = curl_init("http://www.google.es");
+          $response = curl_exec ($handler);  
+          curl_close($handler);
+          echo $response;
           require VIEWS . 'home/index.php';
       }
 
