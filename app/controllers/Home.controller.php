d084407c0edba6350dad993cc147cfee4ad113b8
Ruben Cid <rubencidlara@gmail.com>
2016-01-20 14:43:48 +0100

    pdf

--- app/controllers/Home.controller.php
+++ app/controllers/Home.controller.php
@@ -76,7 +76,7 @@
 
       public function fbAction($a)
       {
-          var_dump($a);
+          // var_dump($a);
           // $fb = new FacebookLogin();
           echo self::$fb->getToken();
 
