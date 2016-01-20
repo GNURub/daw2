7837d7bf5d2035cd3ee965fdb579cfd9f6141715
Ruben Cid <rubencidlara@gmail.com>
2016-01-20 14:17:05 +0100

    fb

--- app/helpers/fb.php
+++ app/helpers/fb.php
@@ -20,17 +20,17 @@ class FacebookLogin
   }
 
   function getToken(){
-    try {
-      $accessToken = $this->helper->getAccessToken();
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      // When Graph returns an error
-      echo 'Graph returned an error: ' . $e->getMessage();
-      exit;
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      // When validation fails or other local issues
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
-      exit;
-    }
+    // try {
+    //   $accessToken = $this->helper->getAccessToken();
+    // } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    //   // When Graph returns an error
+    //   echo 'Graph returned an error: ' . $e->getMessage();
+    //   exit;
+    // } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    //   // When validation fails or other local issues
+    //   echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    //   exit;
+    // }
 
     // if (! isset($accessToken)) {
     //   if ($this->helper->getError()) {
@@ -46,39 +46,39 @@ class FacebookLogin
     //   exit;
     // }
 
-    // Logged in
-    echo '<h3>Access Token</h3>';
-    var_dump($accessToken->getValue());
-
-    // The OAuth 2.0 client handler helps us manage access tokens
-    $oAuth2Client = $this->fb->getOAuth2Client();
-
-    // Get the access token metadata from /debug_token
-    $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-    echo '<h3>Metadata</h3>';
-    var_dump($tokenMetadata);
-
-    // Validation (these will throw FacebookSDKException's when they fail)
-    $tokenMetadata->validateAppId($config['app_id']);
-    // If you know the user ID this access token belongs to, you can validate it here
-    //$tokenMetadata->validateUserId('123');
-    $tokenMetadata->validateExpiration();
-
-    if (! $accessToken->isLongLived()) {
-      // Exchanges a short-lived access token for a long-lived one
-      try {
-        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-      } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        echo "<p>Error getting long-lived access token: " . $this->helper->getMessage() . "</p>\n\n";
-        exit;
-      }
-
-      echo '<h3>Long-lived</h3>';
-      var_dump($accessToken->getValue());
-    }
-
-    $_SESSION['fb_access_token'] = (string) $accessToken;
-  }
+  //   // Logged in
+  //   echo '<h3>Access Token</h3>';
+  //   var_dump($accessToken->getValue());
+  //
+  //   // The OAuth 2.0 client handler helps us manage access tokens
+  //   $oAuth2Client = $this->fb->getOAuth2Client();
+  //
+  //   // Get the access token metadata from /debug_token
+  //   $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+  //   echo '<h3>Metadata</h3>';
+  //   var_dump($tokenMetadata);
+  //
+  //   // Validation (these will throw FacebookSDKException's when they fail)
+  //   $tokenMetadata->validateAppId($config['app_id']);
+  //   // If you know the user ID this access token belongs to, you can validate it here
+  //   //$tokenMetadata->validateUserId('123');
+  //   $tokenMetadata->validateExpiration();
+  //
+  //   if (! $accessToken->isLongLived()) {
+  //     // Exchanges a short-lived access token for a long-lived one
+  //     try {
+  //       $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+  //     } catch (Facebook\Exceptions\FacebookSDKException $e) {
+  //       echo "<p>Error getting long-lived access token: " . $this->helper->getMessage() . "</p>\n\n";
+  //       exit;
+  //     }
+  //
+  //     echo '<h3>Long-lived</h3>';
+  //     var_dump($accessToken->getValue());
+  //   }
+  //
+  //   $_SESSION['fb_access_token'] = (string) $accessToken;
+  // }
 }
 
  ?>
