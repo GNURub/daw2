2ebe8912ac15306d5e6b1a568b8c2c6b423fd417
Ruben Cid <rubencidlara@gmail.com>
2016-01-20 15:09:34 +0100

    fb

--- app/helpers/fb.php
+++ app/helpers/fb.php
@@ -48,36 +48,52 @@ class FacebookLogin
 
     // Logged in
     echo '<h3>Access Token</h3>';
-    var_dump($accessToken->getValue());
+    // var_dump($accessToken->getValue());
+    $this->fb->setDefaultAccessToken($accessToken->getValue());
 
     // The OAuth 2.0 client handler helps us manage access tokens
     $oAuth2Client = $this->fb->getOAuth2Client();
 
-    // Get the access token metadata from /debug_token
-    $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-    echo '<h3>Metadata</h3>';
-    var_dump($tokenMetadata);
-
-    // Validation (these will throw FacebookSDKException's when they fail)
-    $tokenMetadata->validateAppId(FACEBOOK_ID);
-    // If you know the user ID this access token belongs to, you can validate it here
-    //$tokenMetadata->validateUserId('123');
-    $tokenMetadata->validateExpiration();
-
-    if (! $accessToken->isLongLived()) {
-      // Exchanges a short-lived access token for a long-lived one
-      try {
-        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-      } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        echo "<p>Error getting long-lived access token: " . $this->helper->getMessage() . "</p>\n\n";
-        exit;
-      }
-
-      echo '<h3>Long-lived</h3>';
-      var_dump($accessToken->getValue());
+    try {
+      $response = $this->fb->get('/me');
+      $userNode = $response->getGraphUser();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+      echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // When validation fails or other local issues
+      echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
     }
 
-    $_SESSION['fb_access_token'] = (string) $accessToken;
+    echo 'Logged in as ' . $userNode->getName();
+
+    // // // Get the access token metadata from /debug_token
+    // // $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+    // // echo '<h3>Metadata</h3>';
+    // // var_dump($tokenMetadata);
+    //
+    // // Validation (these will throw FacebookSDKException's when they fail)
+    // $tokenMetadata->validateAppId(FACEBOOK_ID);
+    // // If you know the user ID this access token belongs to, you can validate it here
+    // //$tokenMetadata->validateUserId('123');
+    // $tokenMetadata->validateExpiration();
+    //
+    // if (! $accessToken->isLongLived()) {
+    //   // Exchanges a short-lived access token for a long-lived one
+    //   try {
+    //     $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+    //   } catch (Facebook\Exceptions\FacebookSDKException $e) {
+    //     echo "<p>Error getting long-lived access token: " . $this->helper->getMessage() . "</p>\n\n";
+    //     exit;
+    //   }
+    //
+    //   echo '<h3>Long-lived</h3>';
+    //   var_dump($accessToken->getValue());
+    // }
+    //
+    // $_SESSION['fb_access_token'] = (string) $accessToken;
   }
 }
 
